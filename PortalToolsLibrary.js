e39b42d0307c49dbab030dbae89869757076c079
Measomorph <122525998+Measomorph@users.noreply.github.com>
2023-07-12 13:15:08 +0100

    UpdatePortalToolsLibrary.js

--- PortalToolsLibrary.js
+++ PortalToolsLibrary.js
@@ -1,18 +1,20 @@
- // Function to disable a specific lookup
-export function disableLookup(string id){
-        // Disable the element with the given ID
-        $("#" + id).attr("disabled","disabled");
+// Import jQuery
+import $ from 'jquery';
 
-        // Disable the associated button
-        $("#" + id).siblings("div.input-group-btn").find("button").prop("disabled", true);
+// Function to disable a specific lookup
+export function disableLookup(id){
+    // Disable the element with the given ID
+    $("#" + id).attr("disabled","disabled");
 
-        // Hide the associated button
-        $("#" + id).siblings("div.input-group-btn").hide();
-};
+    // Disable the associated button
+    $("#" + id).siblings("div.input-group-btn").find("button").prop("disabled", true);
 
-    // Function to hide all elements with a specific class
-export function hideElementsByClass{string classname}{
-        // Hide all elements with the given class
-        $("." + className).hide();
-};
+    // Hide the associated button
+    $("#" + id).siblings("div.input-group-btn").hide();
+}
 
+// Function to hide all elements with a specific class
+export function hideElementsByClass(className){
+    // Hide all elements with the given class
+    $("." + className).hide();
+}
